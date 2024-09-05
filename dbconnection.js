174130974298d5937b7775935219649aaf7ab92f
marcosolenka <marcosolenka@gmail.com>
2024-09-05 10:09:21 -0300

    added .env archive and dbconnect

---
+++ dbconnection.js
@@ -0,0 +1,30 @@
+import oracledb from "oracledb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function getConnection() {
+  try {
+    const libDir = process.env.ORACLE_CLIENT_LIB_DIR;
+    const user = process.env.ORACLE_USER;
+    const password = process.env.ORACLE_PASSWORD;
+    const connectString = process.env.ORACLE_CONNECT_STRING;
+
+    if (!libDir) {
+      throw new Error("Variável de ambiente ORACLE não definida");
+    }
+
+    oracledb.initOracleClient({ libDIr });
+
+    const connection = await oracledb.getConnection({
+      user: user,
+      password: password,
+      connectString: connectString,
+    });
+    return connection;
+  } catch (err) {
+    throw err;
+  }
+}
+
+//module.exports = { getConnection };
